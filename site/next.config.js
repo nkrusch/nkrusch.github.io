0a57adc99d62d45f11c2e1587627c4673449142d
Neea Rusch <neearusch@gmail.com>
2021-06-02 23:16:16 -0400

    use different path

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,6 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    basePath: debug ? '' : '/nrusch',
-    assetsPrefix: debug ? '' :'/nrusch/'
+    basePath: debug ? '' : '',     /* e.g. /nrusch   (no trailing) */
+    assetsPrefix: debug ? '' : ''  /* e.g. /nrusch/  (with trailing) */
 }
