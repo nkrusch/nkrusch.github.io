0318fd5d71afa0d5b739cced73ce14b6b33bdf97
Neea <neearusch@gmail.com>
2022-05-18 22:09:24 -0400

    sitemap: add cv

--- site/next-sitemap.js
+++ site/next-sitemap.js
@@ -4,4 +4,6 @@ module.exports = {
     siteUrl: process.env.SITE_URL,
     generateRobotsTxt: true,
     changefreq: 'weekly',
+    additionalPaths: async (config) =>
+        [await config.transform(config, 'cv.pdf')]
 }
