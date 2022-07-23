be3e27e4a6adadf53606a0a1dffacb1bb9447180
Neea <neearusch@gmail.com>
2022-07-23 18:13:26 -0400

    edit sitemap

--- site/next-sitemap.js
+++ site/next-sitemap.js
@@ -4,6 +4,11 @@ module.exports = {
     siteUrl: process.env.SITE_URL,
     generateRobotsTxt: true,
     changefreq: 'weekly',
-    additionalPaths: async (config) =>
-        [await config.transform(config, 'cv.pdf')]
+    additionalPaths: async (config) => [
+        await config.transform(config, 'cv.pdf'),
+        await config.transform(config, 'exam_doc.pdf'),
+        await config.transform(config, 'exam_slides.pdf'),
+        await config.transform(config, 'types22_slides.pdf'),
+        await config.transform(config, 'types22_paper.pdf')
+    ]
 }
