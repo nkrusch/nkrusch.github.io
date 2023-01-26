45b2effcc6db70a7e0743f48289c5167311b0291
Neea <neearusch@gmail.com>
2023-01-25 23:24:24 -0500

    update website

--- site/components/pubs.js
+++ site/components/pubs.js
@@ -1,12 +1,10 @@
 import Href from './extLink'
 import utilStyles from '../styles/utils.module.css'
-import Image from 'next/image'
-import doiIcon from '../img/doi.svg'
 
 const Pub = (
     {
         id, title, url, venue, venue_url, author, paper, slides, code,
-        demo, artifact, preprint, doi
+        demo, artifact, preprint
     }) => {
     return <div key={id} className={utilStyles.itemListItem}>
         <div>
@@ -18,7 +16,7 @@ const Pub = (
                 </> : null}.
         </div>
         <div>{author}</div>
-        {(paper || slides || code || demo || artifact || preprint || doi) ?
+        {(paper || slides || code || demo || artifact || preprint) ?
             <div className={utilStyles.pubLinks}>
                 {paper ? <Href href={paper} label="paper" pref={'['}
                 post={']'} className={utilStyles.pubLinks_a}/> : null}
