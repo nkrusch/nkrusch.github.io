abf8a14912a11f81f98d7d04184d1ed66ff24c8a
Neea <neearusch@gmail.com>
2023-05-13 19:45:59 -0400

    try fix duplicate w/ robots; update sitemap

--- site/next-sitemap.js
+++ site/next-sitemap.js
@@ -6,8 +6,18 @@ module.exports = {
     changefreq: 'weekly',
     additionalPaths: async (config) => [
         await config.transform(config, 'cv.pdf'),
-        await config.transform(config, 'files/splashcomp22-p10.pdf'),
+        await config.transform(config, 'files/coqpl_abstract.pdf'),
+        await config.transform(config, 'files/exam_doc.pdf'),
+        await config.transform(config, 'files/exam_slides.pdf'),
+        await config.transform(config, 'files/grd_poster.pdf'),
         await config.transform(config, 'files/mwp_slides.pdf'),
-        await config.transform(config, 'files/grd_poster.pdf')
+        await config.transform(config, 'files/proposal.pdf'),
+        await config.transform(config, 'files/proposal_slides.pdf'),
+        await config.transform(config, 'files/scot.pdf'),
+        await config.transform(config, 'files/splashcomp22-p10.pdf'),
+        await config.transform(config, 'files/types_paper.pdf'),
+        await config.transform(config, 'files/types_slides.pdf'),
+        await config.transform(config, 'files/vmcai_paper.pdf'),
+        await config.transform(config, 'files/vmcai_slides.pdf'),
     ]
 }
